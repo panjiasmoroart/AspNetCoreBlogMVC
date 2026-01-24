5699ff26d53f17357577c15f96704aec8018ac83
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-24 17:14:45 +0700

    ASP.NET MVC Web - Creating the DbContext Class

---
+++ AspNetCoreBlogMVC/Data/BlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreBlogMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreBlogMVC.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+		public DbSet<BlogPost> BlogPosts { get; set; }
+		public DbSet<Tag> Tags { get; set; }
+	}
+}
