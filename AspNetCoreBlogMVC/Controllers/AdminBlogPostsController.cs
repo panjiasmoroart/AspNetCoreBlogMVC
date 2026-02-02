b5b74e98c49c66e49e732cffa44126ae029cbc86
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-02-02 10:39:44 +0700

    AdminBlogPosts CRUD - Create New Controller (AdminBlogPosts), Add Action Method (GET)

---
+++ AspNetCoreBlogMVC/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBlogMVC.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
