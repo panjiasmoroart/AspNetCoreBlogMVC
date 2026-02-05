00e7d50672fad53d0612a55dee307dbfa9a3e943
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-02-05 21:21:41 +0700

    Adding WYSIWYG Editor (Froala) - Create Image API

---
+++ AspNetCoreBlogMVC/Controllers/ImagesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBlogMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+		
+		[HttpGet]
+        public IActionResult Index()
+        {
+			//https://localhost:7194/api/images
+			return Ok("This is the GET Images API call");
+		}
+	}
+}
