8c1641332ff0873be873c093ab30fa8dc01be910
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-02-10 12:12:58 +0700

    Authentication and Authorization - Login User Functionality

--- AspNetCoreBlogMVC/Controllers/AccountController.cs
+++ AspNetCoreBlogMVC/Controllers/AccountController.cs
@@ -4,41 +4,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreBlogMVC.Controllers
 {
-    public class AccountController : Controller
-    {
-        private readonly UserManager<IdentityUser> userManager;
+	public class AccountController : Controller
+	{
+		private readonly UserManager<IdentityUser> userManager;
+		private readonly SignInManager<IdentityUser> signInManager;
 
 		public AccountController(UserManager<IdentityUser> userManager,
 			SignInManager<IdentityUser> signInManager)
 		{
 			this.userManager = userManager;
+			this.signInManager = signInManager;
 		}
 
 
 		[HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
+		public IActionResult Register()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                var identityUser = new IdentityUser
-                {
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+		{
+			if (ModelState.IsValid)
+			{
+				var identityUser = new IdentityUser
+				{
 					UserName = registerViewModel.Username,
 					Email = registerViewModel.Email
 				};
 
-                var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
-                if (identityResult.Succeeded)
-                {
+				var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
+				if (identityResult.Succeeded)
+				{
 					// assign this user the "User" role
-                    var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
-                    if (roleIdentityResult.Succeeded)
-                    {
+					var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
+					if (roleIdentityResult.Succeeded)
+					{
 						// Show success notification
 						return RedirectToAction("Register");
 					}
@@ -46,6 +48,35 @@ namespace AspNetCoreBlogMVC.Controllers
 			}
 
 			// Show error notification
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Login(string ReturnUrl)
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			var signInResult = await signInManager.PasswordSignInAsync(loginViewModel.Username,
+			   loginViewModel.Password, false, false);
+
+			if (signInResult != null && signInResult.Succeeded)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			// gunakan string.Empty agar muncul di asp-validation-summary="ModelOnly"
+			ModelState.AddModelError(string.Empty, "Username atau password salah.");
+
+			// Show errors
 			return View();
 		}
 
