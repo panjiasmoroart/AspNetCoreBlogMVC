04e94375bead2bd5800f4780a983e4b1a15cbfbf
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-04-10 13:45:51 +0700

    Manage Users - Delete User

--- AspNetCoreBlogMVC/Controllers/AdminUsersController.cs
+++ AspNetCoreBlogMVC/Controllers/AdminUsersController.cs
@@ -76,5 +76,23 @@ namespace AspNetCoreBlogMVC.Controllers
 
 			return View();
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> Delete(Guid id)
+		{
+			var user = await userManager.FindByIdAsync(id.ToString());
+
+			if (user is not null)
+			{
+				var identityResult = await userManager.DeleteAsync(user);
+
+				if (identityResult is not null && identityResult.Succeeded)
+				{
+					return RedirectToAction("List", "AdminUsers");
+				}
+			}
+
+			return View();
+		}
 	}
 }
